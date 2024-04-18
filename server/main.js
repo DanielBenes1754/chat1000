8689a04ed1972151d414bd68f7f669ab7aec64fb
KP <KP@PCFEIB208-90.msad.vsb.cz>
2024-04-18 17:00:56 +0200

    nic

--- server/main.js
+++ server/main.js
@@ -6,13 +6,15 @@ const io = new Server(3000, { cors: { origin: '*' } });
 io.on('connection', (socket) => {
   console.log('A user connected');
 
+
   socket.on('disconnect', () => {
     console.log('User disconnected');
   });
 
   socket.on('chat_message', (msg) => {
-    console.log('message: ' + msg);
-    io.emit('chat_message', msg);
+    console.log('message: ' + msg.text);
+    socket.join(msg.room)
+    io.to(msg.room).emit('chat_message', msg.text);
   });
 });
 
